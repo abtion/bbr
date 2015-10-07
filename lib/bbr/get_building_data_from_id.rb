95749844ea453337fe574c5b9296367a576eda4a
Nicolai Seerup <mail@soundtracktor.com>
2015-10-07 12:12:11 +0200

    remove output formatting

--- lib/bbr/get_building_data_from_id.rb
+++ lib/bbr/get_building_data_from_id.rb
@@ -25,63 +25,16 @@ module BBR
           building_area: response.at_xpath("//BebyggetAreal").try(:content),
           building_age: (Time.now.year - response.at_xpath("//Opfoerselsaar").try(:content).to_i).to_i,
           roof_type: response.at_xpath("//Tagdaekningsmateriale/Value").try(:content),
-          roof_tilt: roof_tilt(response),
           heating: response.at_xpath("//Varmeinstallation/Value").try(:content),
           heating_source: response.at_xpath("//Opvarmningsmiddel/Value").try(:content),
-          formatted_heating: formatted_heating(response),
-          outer_wall_material: response.at_xpath("//Ydervaegsmateriale/Value").try(:content)
+          outer_wall_material: response.at_xpath("//Ydervaegsmateriale/Value").try(:content),
+          floors: response.at_xpath("//AntalEtager").try(:content),
+          material: response.at_xpath("//Tagdaekningsmateriale/Value").try(:content),
+          roof_area: response.at_xpath("//UdnyttetTagetageareal").try(:content)
         }
       else
         Hash.new
       end
     end
-
-    def self.roof_tilt(response)
-      floors    = response.at_xpath("//AntalEtager").try :content
-      material  = response.at_xpath("//Tagdaekningsmateriale/Value").try :content
-      roof_area = response.at_xpath("//UdnyttetTagetageareal").try :content
-
-      if floors
-        t = floors == 1 ? 2 : 3
-        if material == 'BuiltUp'
-          t = 1
-        elsif roof_area.to_i > 0
-          t = 3
-        end
-        t
-      else
-        2
-      end
-    end
-
-    def self.formatted_heating(response)
-      heating         = response.at_xpath("//Varmeinstallation/Value").try :content
-      heating_source  = response.at_xpath("//Opvarmningsmiddel/Value").try :content
-
-      case heating
-      when 'FjernvarmeBlokvarme'
-        'Fjernvarme'
-      when 'ElovneElpaneler'
-        'Elvarme'
-      when 'Varmepumpe'
-        'Varmepumpe'
-      when 'Gasradiator'
-        'Gas'
-      when 'CentralvarmeFraEgetAnlaegEtKammerFyr', 'CentralvarmeMedToFyringsenheder', 'Ovne'
-        case heating_source
-        when 'Elektricitet'
-          'Elvarme'
-        when 'Gasvaerksgas', 'Naturgas'
-          'Gas'
-        when 'FastBraendsel', 'Halm'
-          'Træpillefyr/Halmfyr'
-        else
-          'Oliefyr'
-        end
-      else
-        "#{ heating } / #{ heating_source }"
-      end
-    end
   end
-
 end
